74da56cd67deea4ab66f963a7a2e9156efdf4a6e
sara woodford <sarawoodford6@gmail.com>
2024-12-05 00:24:31 -0330

    feat: Product DOA
    implemented methods for CURD operation
    - add a product
    - list all existing products
    - update an existing product
    - delete a product
    - javadocs for readability/clarity in code

--- src/main/java/ecommerce/dao/ProductDAO.java
+++ src/main/java/ecommerce/dao/ProductDAO.java
@@ -1,7 +1,101 @@
 package ecommerce.dao;
 
-// Data Access Object (DAO) for managing Product data in the database.
-// Contains methods for CRUD operations related to products.
+import ecommerce.model.Product;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Product DAO handles and manages Product objects in the database
+ * 
+ * methods are created to perform CURD
+ */
 public class ProductDAO {
-    // Methods for creating, reading, updating, and deleting products in the database.
+    /**
+     * database url, username and password for connection
+     */
+    private static final String URL = "";
+    private static final String USER = "sara";
+    private static final String PASSWORD = "sara";
+
+    /**
+     * adds a new product to the products table
+     * 
+     * @param product
+     */
+    public void addProduct(Product product) {
+        String query = "INSERT INTO products (name, price, quantity, seller_id) VALUES (" +
+                product.getProductName() + ", " +
+                product.getProductPrice() + ", " +
+                product.getProductStock() + ", " +
+                product.getProductSellerId() + ")";
+
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                Statement statement = connection.createStatement()) {
+            statement.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * returns a list of all products in the products table
+     * 
+     * @return all products
+     */
+    public List<Product> getProducts() {
+        List<Product> products = new ArrayList<>();
+        String query = "SELECT * FROM products";
+
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(query)) {
+            while (resultSet.next()) {
+                products.add(new Product(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getDouble("price"),
+                        resultSet.getInt("quantity"),
+                        resultSet.getInt("seller_id")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return products;
+    }
+
+    /**
+     * updates the fields of an existing product in the products table
+     * 
+     * @param product
+     */
+    public void updateProduct(Product product) {
+        String query = "UPDATE products SET name = '" + product.getProductName() + "', " +
+                "price = " + product.getProductPrice() + ", " +
+                "quantity = " + product.getProductStock() + " " +
+                "WHERE id = " + product.getProductId();
+
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                Statement statement = connection.createStatement()) {
+            statement.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * deletes a product from the products table by using productId attribute
+     * 
+     * @param productId
+     */
+    public void deleteProduct(int productId) {
+        String query = "DELETE FROM products WHERE id = " + productId;
+
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                Statement statement = connection.createStatement()) {
+            statement.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
