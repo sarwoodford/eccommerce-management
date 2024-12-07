e95d501d6c50570bed6db80a7a9ec8d3d01dd465
SearchingSteve <155838264+SearchingSteve@users.noreply.github.com>
2024-12-06 22:08:10 -0330

    feat: Extended User classes
    Added extended user classes: Seller & Admin with constructors.

--- src/main/java/ecommerce/model/Admin.java
+++ src/main/java/ecommerce/model/Admin.java
@@ -3,4 +3,9 @@ package ecommerce.model;
 // Inherits from User and contains specific functionalities for administrators.
 public class Admin extends User {
     // Additional methods specific to admins, e.g., managing users and products.
+       // Constructor
+       public Admin(int id, String username, String password, String email) {
+        super(id, username, password, email, "admin");
+    }
+
 }
