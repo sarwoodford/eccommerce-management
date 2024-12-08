3b223ae2126b1d14783d776fd52befcb177f9e4d
sara woodford <sarawoodford6@gmail.com>
2024-12-08 18:03:23 -0330

    feat: productService class for CURD functionality
    productService utilizes methods from Product and
    ProductDAO. productService methods include:
    - listing all products in database
    - add a new product
    - update an existing product
    - delete a product from the database based on
    productId
    - display all products sold by a specific seller
    based on sellerId

--- src/main/java/ecommerce/service/ProductService.java
+++ src/main/java/ecommerce/service/ProductService.java
@@ -1,9 +1,43 @@
 package ecommerce.service;
 
+import java.util.List;
+
+import ecommerce.dao.ProductDAO;
+import ecommerce.model.Product;
+
 // Service class for managing product-related business logic.
 // This class interacts with ProductDAO to perform operations.
 public class ProductService {
-    // Method to add a new product.
-    // Method to update an existing product.
-    // Method to delete a product.
+    private final ProductDAO productDAO;
+
+    public ProductService() {
+        this.productDAO = new ProductDAO();
+    }
+
+    public List<Product> getAllProducts() {
+        return productDAO.getProducts();
+    }
+
+    public void addProduct(Product product) {
+        if (product.getProductPrice() < 0 || product.getProductStock() < 0) {
+            throw new IllegalArgumentException("Price and stock quantity must be greater than ($)0.");
+        }
+        productDAO.addProduct(product);
+    }
+
+    public void updateProduct(Product product) {
+        if (product.getProductPrice() < 0 || product.getProductStock() < 0) {
+            throw new IllegalArgumentException("Price and stock quantity must be greater than ($)0.");
+        }
+        productDAO.updateProduct(product);
+    }
+
+    public void deleteProduct(int productId) {
+        productDAO.deleteProduct(productId);
+    }
+
+    public List<Product> getSellerProducts(int productSellerId) {
+        return productDAO.getProducts().stream().filter(product -> product.getProductSellerId() == productSellerId)
+                .toList();
+    }
 }
