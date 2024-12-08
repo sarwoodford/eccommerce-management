f744e361b63859eb692daca834bf0845f2ec7401
sara woodford <sarawoodford6@gmail.com>
2024-12-08 18:10:49 -0330

    docs: add javadocs to ProductService
    added descriptions for all methods for code clarity
    and readability

--- src/main/java/ecommerce/service/ProductService.java
+++ src/main/java/ecommerce/service/ProductService.java
@@ -5,19 +5,38 @@ import java.util.List;
 import ecommerce.dao.ProductDAO;
 import ecommerce.model.Product;
 
-// Service class for managing product-related business logic.
-// This class interacts with ProductDAO to perform operations.
+/**
+ * ProductService manages products in the ecommerce system by interacting with
+ * ProductDAO
+ */
 public class ProductService {
+    /**
+     * ProductDAO instance assigned
+     */
     private final ProductDAO productDAO;
 
+    /**
+     * Constructor initializes the ProductDAO
+     */
     public ProductService() {
         this.productDAO = new ProductDAO();
     }
 
+    /**
+     * Lists all products in the ecommerce database
+     * 
+     * @return list of products
+     */
     public List<Product> getAllProducts() {
         return productDAO.getProducts();
     }
 
+    /**
+     * adds a new product to the database ensuring that stock and price are not
+     * a negative number
+     * 
+     * @param product
+     */
     public void addProduct(Product product) {
         if (product.getProductPrice() < 0 || product.getProductStock() < 0) {
             throw new IllegalArgumentException("Price and stock quantity must be greater than ($)0.");
@@ -25,6 +44,12 @@ public class ProductService {
         productDAO.addProduct(product);
     }
 
+    /**
+     * updates an existing product in the database ensuring that price and
+     * stock are not negative numbers
+     * 
+     * @param product
+     */
     public void updateProduct(Product product) {
         if (product.getProductPrice() < 0 || product.getProductStock() < 0) {
             throw new IllegalArgumentException("Price and stock quantity must be greater than ($)0.");
@@ -32,10 +57,22 @@ public class ProductService {
         productDAO.updateProduct(product);
     }
 
+    /**
+     * deletes a product from the database using product id
+     * 
+     * @param productId
+     */
     public void deleteProduct(int productId) {
         productDAO.deleteProduct(productId);
     }
 
+    /**
+     * returns a list of all products affiliated with a specified seller
+     * based on seller id
+     * 
+     * @param productSellerId
+     * @return list of seller products
+     */
     public List<Product> getSellerProducts(int productSellerId) {
         return productDAO.getProducts().stream().filter(product -> product.getProductSellerId() == productSellerId)
                 .toList();
