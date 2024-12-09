306d163779caf67ce557dd706f7987d1d0b4f0ed
sara woodford <sarawoodford6@gmail.com>
2024-12-09 16:19:09 -0330

    feat: UserController
    User controller acts as an intemediate between
    User and Userservice and App.java menu to perform
    CURD operations for users

--- src/main/java/ecommerce/controller/UserController.java
+++ src/main/java/ecommerce/controller/UserController.java
@@ -1,8 +1,71 @@
 package ecommerce.controller;
 
-// Controller for handling user-related actions and requests.
-// This class processes user input and interacts with the UserService.
+import java.util.List;
+
+import ecommerce.model.User;
+import ecommerce.service.UserService;
+import ecommerce.model.Admin;
+import ecommerce.model.Buyer;
+import ecommerce.model.Seller;
+
 public class UserController {
-    // Method to handle user registration.
-    // Method to handle user login.
+    private final UserService userService;
+
+    public UserController() {
+        this.userService = new UserService();
+    }
+
+    public void addUser(int id, String username, String password, String email, String role) {
+        User user;
+
+        switch (role.toLowerCase()) {
+            case "admin":
+                user = new Admin(id, username, password, email);
+                break;
+            case "seller":
+                user = new Seller(id, username, password, email);
+                break;
+            case "buyer":
+                user = new Buyer(id, username, password, email);
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid role. Valid user roles include Buyer, Seller and Admin.");
+        }
+        userService.addUser(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    public User getUserByUsername(String username) {
+        return userService.getUserByUsername(username);
+    }
+
+    public void updateUser(int id, String username, String password, String email, String role) {
+        User user;
+
+        switch (role.toLowerCase()) {
+            case "admin":
+                user = new Admin(id, username, password, email);
+                break;
+            case "seller":
+                user = new Seller(id, username, password, email);
+                break;
+            case "buyer":
+                user = new Buyer(id, username, password, email);
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid role. Valid roles include seller, buyer and admin.");
+        }
+        userService.updateUser(user);
+    }
+
+    public void deleteUser(int userId) {
+        userService.deleteUser(userId);
+    }
+
+    public List<User> getUsersByRole(String role) {
+        return userService.getUsersByRole(role);
+    }
 }
