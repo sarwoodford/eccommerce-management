e3b31e8fe71d809cb926aac48e24ec04bc2fdcbd
sara woodford <sarawoodford6@gmail.com>
2024-12-09 16:30:38 -0330

    docs: javadocs for UserService and UserController
    javadocs provided for code clarity and readability

--- src/main/java/ecommerce/controller/UserController.java
+++ src/main/java/ecommerce/controller/UserController.java
@@ -8,13 +8,32 @@ import ecommerce.model.Admin;
 import ecommerce.model.Buyer;
 import ecommerce.model.Seller;
 
+/**
+ * UserController class acts as an intermediate between User and UserService
+ * classes, and the main menu entry point in App.java to manage CURD operations
+ * for users.
+ * 
+ */
 public class UserController {
     private final UserService userService;
 
+    /**
+     * Constructor for UserController
+     */
     public UserController() {
         this.userService = new UserService();
     }
 
+    /**
+     * adds a new user, including a switch case which only accepts valid roles
+     * and adds a new User object based on which role is entered
+     * 
+     * @param id
+     * @param username
+     * @param password
+     * @param email
+     * @param role
+     */
     public void addUser(int id, String username, String password, String email, String role) {
         User user;
 
@@ -34,14 +53,36 @@ public class UserController {
         userService.addUser(user);
     }
 
+    /**
+     * returns list of all users in database
+     * 
+     * @return List of users
+     */
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
+    /**
+     * retrieves user based on their username
+     * 
+     * @param username
+     * @return user
+     */
     public User getUserByUsername(String username) {
         return userService.getUserByUsername(username);
     }
 
+    /**
+     * updates an existing user in the database, including a switch case which
+     * updates the user
+     * depending on which role is entered
+     * 
+     * @param id
+     * @param username
+     * @param password
+     * @param email
+     * @param role
+     */
     public void updateUser(int id, String username, String password, String email, String role) {
         User user;
 
@@ -61,10 +102,20 @@ public class UserController {
         userService.updateUser(user);
     }
 
+    /**
+     * deletes a user based on their unique id
+     * 
+     * @param userId
+     */
     public void deleteUser(int userId) {
         userService.deleteUser(userId);
     }
 
+    /**
+     * returns a list of users under a specified role
+     * 
+     * @return list of users by role
+     */
     public List<User> getUsersByRole(String role) {
         return userService.getUsersByRole(role);
     }
--- src/main/java/ecommerce/service/UserService.java
+++ src/main/java/ecommerce/service/UserService.java
@@ -5,13 +5,24 @@ import java.util.List;
 import ecommerce.dao.UserDAO;
 import ecommerce.model.User;
 
+/**
+ * UserService class utilized User and UserDAO classes for CURD operations
+ */
 public class UserService {
     private final UserDAO userDAO;
 
+    /**
+     * User Service Constructor
+     */
     public UserService() {
         this.userDAO = new UserDAO();
     }
 
+    /**
+     * adds a user which validating empty fields that are required
+     * 
+     * @param user
+     */
     public void addUser(User user) {
         if (user.getUsername() == null || user.getUsername().isEmpty()) {
             throw new IllegalArgumentException("Username cannot be empty. Please try again.");
@@ -22,22 +33,49 @@ public class UserService {
         userDAO.addUser(user);
     }
 
+    /**
+     * returns a list of all users in database
+     * 
+     * @return list of all users
+     */
     public List<User> getAllUsers() {
         return userDAO.getAllUsers();
     }
 
+    /**
+     * returns a user by identifying them by their username
+     * 
+     * @param username
+     * @return user based on their username
+     */
     public User getUserByUsername(String username) {
         return userDAO.getUserByUsername(username);
     }
 
+    /**
+     * updates an existing user in the database
+     * 
+     * @param user
+     */
     public void updateUser(User user) {
         userDAO.updateUser(user);
     }
 
+    /**
+     * deletes a user by their unique id
+     * 
+     * @param userId
+     */
     public void deleteUser(int userId) {
         userDAO.deleteUserById(userId);
     }
 
+    /**
+     * returns a list of users given specified role
+     * 
+     * @param role
+     * @return list of all users by role
+     */
     public List<User> getUsersByRole(String role) {
         return userDAO.getAllUsers().stream().filter(user -> user.getRole().equalsIgnoreCase(role)).toList();
     }
