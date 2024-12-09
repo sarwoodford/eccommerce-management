0a2387cd88e137bdf644a3f9f0d96b2cc1228395
sara woodford <sarawoodford6@gmail.com>
2024-12-09 16:33:55 -0330

    docs: javadocs in ProductController
    javadocs provided for code clarity and readability

--- src/main/java/ecommerce/controller/ProductController.java
+++ src/main/java/ecommerce/controller/ProductController.java
@@ -5,29 +5,63 @@ import java.util.List;
 import ecommerce.model.Product;
 import ecommerce.service.ProductService;
 
+/**
+ * product controller acts as an intermediate between Product
+ * and ProductService classes, and App.java - main entry point for
+ * ecommerce menu
+ */
 public class ProductController {
     private final ProductService productService;
 
+    /**
+     * ProductController constructor
+     */
     public ProductController() {
         this.productService = new ProductService();
     }
 
+    /**
+     * returns a list of all products in database
+     * 
+     * @return list of products
+     */
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
 
+    /**
+     * adds a new product to database
+     * 
+     * @param product
+     */
     public void addProduct(Product product) {
         productService.addProduct(product);
     }
 
+    /**
+     * updates an existing product in database
+     * 
+     * @param product
+     */
     public void updateProduct(Product product) {
         productService.updateProduct(product);
     }
 
+    /**
+     * deletes a product by its unique id
+     * 
+     * @param productId
+     */
     public void deleteProdyct(int productId) {
         productService.deleteProduct(productId);
     }
 
+    /**
+     * returns a list of products sold by specified seller
+     * 
+     * @param productSellerId
+     * @return list of seller products
+     */
     public List<Product> getProductsBySeller(int productSellerId) {
         return productService.getSellerProducts(productSellerId);
     }
